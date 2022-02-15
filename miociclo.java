bd862307c250616e1ad4a0fded1612407be07b68
lai2-01 <lai2-01@itismeucci.com>
2022-02-15 11:28:24 +0100

    ho modificato di nuovo il commento

--- miociclo.java
+++ miociclo.java
@@ -2,7 +2,7 @@ public class miociclo{
 	static void main(String[] args){
 		for (int i = 0; i < 10; i++){
 			System.out.println(i);
-			//commento
+			//commento modificato nel branch "master"
 		}
 	}
 }
